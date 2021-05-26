74bfddd00295ab61db185f95d73c9e0d444124ea
Marina <m.gerasymova29@gmail.com>
2021-05-26 23:56:53 +0300

    Create header, footer, above-the-fold

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Header />
+    <Content />
+    <Footer />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import "./style/main.scss";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Content from "./components/FrontPage.vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  components: {
+    Header,
+    Footer,
+    Content,
+  },
+};
+</script>
---
+++ src/components/FrontPage.vue
@@ -0,0 +1,384 @@
+<template>
+  <main class="main">
+    <section class="main__above-the-fold above-the-fold">
+      <div class="above-the-fold__content">
+        <h1 class="above-the-fold__title">
+          SEO & Digital
+          <span class="above-the-fold__title-span">Marketing</span> For Your
+          Website
+        </h1>
+        <p class="above-the-fold__sub-title">
+          Provide all kind of seo services and help to improve seo ranking.
+          Globally incubate standards.
+        </p>
+        <div class="above-the-fold__set-of-buttons">
+          <a href="#" class="above-the-fold__button button">Start now</a>
+          <a href="#" class="above-the-fold__video-button">
+            <div class="above-the-fold__play-icon-wrap">
+              <svg
+                width="10"
+                height="12"
+                viewBox="0 0 10 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="above-the-fold__icon-play"
+              >
+                <path
+                  d="M8.5 4.26795C9.83333 5.03775 9.83333 6.96225 8.5 7.73205L3.25 10.7631C1.91666 11.5329 0.249999 10.5707 0.25 9.03109L0.25 2.96891C0.25 1.42931 1.91667 0.46706 3.25 1.23686L8.5 4.26795Z"
+                  fill="#F25822"
+                />
+              </svg>
+            </div>
+            <span class="above-the-fold__video-button-sign">Watch Video</span>
+          </a>
+        </div>
+      </div>
+      <div class="above-the-fold__image-wrap">
+        <img
+          src="../image/seo-and-digital.svg"
+          alt="SEO and Digital Marketing for your website"
+          class="above-the-fold__image"
+        />
+      </div>
+    </section>
+
+    <section class="main__logos logos">
+      <img
+        src="../image/logo__liva.svg"
+        alt="liva"
+        title="liva"
+        class="logos__logo"
+        style="margin-left: -20px"
+      />
+      <img
+        src="../image/logo__kanba.svg"
+        alt="Kanba"
+        title="Kanba"
+        class="logos__logo"
+      />
+      <img
+        src="../image/logo__amara.svg"
+        alt="Amara"
+        title="Amara"
+        class="logos__logo"
+      />
+      <img
+        src="../image/logo__aven.svg"
+        alt="Aven"
+        title="Aven"
+        class="logos__logo"
+      />
+      <img
+        src="../image/logo__hex-lab.svg"
+        alt="Hexlab"
+        title="Hexlab"
+        class="logos__logo"
+      />
+      <img
+        src="../image/logo__kanba.svg"
+        alt="Kanba"
+        title="Kanba"
+        class="logos__logo"
+      />
+    </section>
+
+    <infinite-slide-bar
+      duration="10s"
+      direction="reverse"
+      delay="8s"
+      style="margin: 50px auto"
+    >
+      <section class="main__logos-mobile logos">
+        <img
+          src="../image/logo__liva.svg"
+          alt="liva"
+          title="liva"
+          class="logos__logo"
+          style="margin-left: -20px"
+        />
+        <img
+          src="../image/logo__kanba.svg"
+          alt="Kanba"
+          title="Kanba"
+          class="logos__logo"
+        />
+        <img
+          src="../image/logo__amara.svg"
+          alt="Amara"
+          title="Amara"
+          class="logos__logo"
+        />
+        <img
+          src="../image/logo__aven.svg"
+          alt="Aven"
+          title="Aven"
+          class="logos__logo"
+        />
+        <img
+          src="../image/logo__hex-lab.svg"
+          alt="Hexlab"
+          title="Hexlab"
+          class="logos__logo"
+        />
+        <img
+          src="../image/logo__kanba.svg"
+          alt="Kanba"
+          title="Kanba"
+          class="logos__logo"
+        />
+      </section>
+    </infinite-slide-bar>
+
+    <article class="main__acricle-wrap main--background-grey">
+      <div class="main__acticle-with-right-picture article-with-right-picture">
+        <div class="article-with-right-picture__content">
+          <h2 class="article-with-right-picture__title">
+            Perfect Solution For Your Business
+          </h2>
+          <p class="article-with-right-picture__sign">
+            Distinctively exploit optimal alignments for intuitive bandwidth.
+            Quickly coordinate e-business applications through revolutionary
+            catalysts for chang the Seamlessly underwhelm optimal testing
+            procedures.
+          </p>
+          <a href="#" class="article-with-right-picture__button button"
+            >Learn More</a
+          >
+        </div>
+        <div class="article-with-right-picture__image-wrap">
+          <img
+            src="../image/macbook.png"
+            alt="Macbook"
+            class="article-with-right-picture__image"
+            style="margin-right: -100px"
+          />
+        </div>
+      </div>
+    </article>
+
+    <section class="main__cards-of-oppotunity">
+      <a href="#" class="main__card-of-oppotunity card-of-oppotunity">
+        <div
+          class="
+            card-of-oppotunity__icon-wrap card-of-oppotunity__icon-wrap--red
+          "
+        >
+          <svg
+            width="28"
+            height="25"
+            viewBox="0 0 28 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.77778 2.77778V22.2222H25V2.77778H2.77778ZM1.38889 0H26.3889C26.7572 0 27.1105 0.146329 27.371 0.406796C27.6314 0.667263 27.7778 1.02053 27.7778 1.38889V23.6111C27.7778 23.9795 27.6314 24.3327 27.371 24.5932C27.1105 24.8537 26.7572 25 26.3889 25H1.38889C1.02053 25 0.667263 24.8537 0.406796 24.5932C0.146329 24.3327 0 23.9795 0 23.6111V1.38889C0 1.02053 0.146329 0.667263 0.406796 0.406796C0.667263 0.146329 1.02053 0 1.38889 0ZM17.7681 9.43472L15.2778 6.94444H22.2222V13.8889L19.7319 11.3986L14.3653 16.7653L11.4194 13.8194L7.49028 17.7472L5.52639 15.7833L11.4194 9.89028L14.3653 12.8375L17.7681 9.43472Z"
+              fill="#F25822"
+            />
+          </svg>
+        </div>
+        <div>
+          <h4 class="card-of-oppotunity__title">Grow Your Business</h4>
+          <p class="card-of-oppotunity__sign">
+            Leverage agile frameworks to provide a robust synopsis for high
+            level overviews.
+          </p>
+          <p
+            class="card-of-oppotunity__link card-of-oppotunity__link--hover-red"
+          >
+            Read more
+          </p>
+        </div>
+      </a>
+
+      <a href="#" class="main__card-of-oppotunity card-of-oppotunity">
+        <div
+          class="
+            card-of-oppotunity__icon-wrap card-of-oppotunity__icon-wrap--green
+          "
+        >
+          <svg
+            width="17"
+            height="23"
+            viewBox="0 0 17 23"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.57291 22.9583C7.45213 22.9581 7.33229 22.937 7.21875 22.8958C6.99973 22.8156 6.81397 22.6642 6.6912 22.4659C6.56844 22.2676 6.51578 22.0338 6.54166 21.802L7.34375 14.4166H1.20833C1.01909 14.4169 0.833349 14.3656 0.671053 14.2683C0.508758 14.171 0.376039 14.0313 0.287152 13.8642C0.198266 13.6972 0.156569 13.509 0.16654 13.3201C0.176511 13.1311 0.237773 12.9484 0.343747 12.7916L8.5625 0.499938C8.69248 0.307582 8.883 0.164162 9.1038 0.0924479C9.32461 0.020734 9.56304 0.0248367 9.78125 0.104105C9.99102 0.181983 10.1701 0.325339 10.2921 0.512949C10.414 0.700559 10.4723 0.92245 10.4583 1.14577L9.65625 8.58327H15.7917C15.9809 8.58299 16.1666 8.63426 16.3289 8.73158C16.4912 8.8289 16.624 8.96859 16.7128 9.13566C16.8017 9.30272 16.8434 9.49085 16.8335 9.67982C16.8235 9.8688 16.7622 10.0515 16.6562 10.2083L8.4375 22.4999C8.34194 22.6413 8.21313 22.757 8.06237 22.8369C7.91162 22.9169 7.74354 22.9585 7.57291 22.9583ZM3.15625 12.3333H8.5C8.64535 12.3336 8.78903 12.3644 8.92179 12.4236C9.05454 12.4828 9.17344 12.5691 9.27083 12.677C9.36913 12.7864 9.44316 12.9153 9.48804 13.0554C9.53293 13.1954 9.54765 13.3434 9.53125 13.4895L9.0625 17.8124L13.8437 10.6666H8.5C8.353 10.6675 8.20749 10.6373 8.07301 10.5779C7.93853 10.5185 7.81814 10.4314 7.71975 10.3222C7.62137 10.213 7.5472 10.0842 7.50215 9.94425C7.45709 9.80433 7.44216 9.65646 7.45833 9.51035L7.92708 5.18744L3.15625 12.3333Z"
+              fill="#149211"
+            />
+          </svg>
+        </div>
+        <div>
+          <h4 class="card-of-oppotunity__title">Get Quality Ieads</h4>
+          <p class="card-of-oppotunity__sign">
+            Leverage agile frameworks to provide a robust synopsis for high
+            level overviews.
+          </p>
+          <p
+            class="
+              card-of-oppotunity__link card-of-oppotunity__link--hover-green
+            "
+          >
+            Read more
+          </p>
+        </div>
+      </a>
+
+      <a href="#" class="main__card-of-oppotunity card-of-oppotunity">
+        <div
+          class="
+            card-of-oppotunity__icon-wrap card-of-oppotunity__icon-wrap--yellow
+          "
+        >
+          <svg
+            width="19"
+            height="19"
+            viewBox="0 0 19 19"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.40625 18.9167C8.90325 18.9162 8.42062 18.7179 8.0625 18.3646L1.38542 11.6771C1.21408 11.5068 1.10729 11.2822 1.08334 11.0417L0.166671 1.30215C0.152638 1.14958 0.172435 0.99579 0.224651 0.851747C0.276866 0.707704 0.360214 0.576953 0.468754 0.468815C0.576892 0.360275 0.707643 0.276927 0.851686 0.224712C0.995729 0.172497 1.14952 0.152699 1.30209 0.166732L11.0417 1.0834C11.2821 1.10735 11.5067 1.21414 11.6771 1.38548L18.3646 8.06256C18.7189 8.41794 18.9179 8.89929 18.9179 9.40111C18.9179 9.90293 18.7189 10.3843 18.3646 10.7396L10.7396 18.3646C10.3853 18.7173 9.9061 18.9157 9.40625 18.9167ZM3.15625 10.4688L9.40625 16.7188L16.75 9.37506L10.5 3.12506L2.36459 2.36465L3.15625 10.4688Z"
+              fill="#FFC700"
+            />
+          </svg>
+        </div>
+        <div>
+          <h4 class="card-of-oppotunity__title">Drive More Sales</h4>
+          <p class="card-of-oppotunity__sign">
+            Leverage agile frameworks to provide a robust synopsis for high
+            level overviews.
+          </p>
+          <p
+            class="
+              card-of-oppotunity__link card-of-oppotunity__link--hover-yellow
+            "
+          >
+            Read more
+          </p>
+        </div>
+      </a>
+    </section>
+
+    <section class="main__why-us-wraper main--background-grey">
+      <div class="main__why-us why-us">
+        <h2 class="why-us__title">Why You Should Choose Us.</h2>
+        <p class="why-us__sign">
+          Distinctively exploit optimal alignments for intuitive bandwidth.
+          Quickly coordinate e-business applications through revolutionary
+          catalysts.
+        </p>
+        <div class="why-us__cards">
+          <div class="why-us__card-of-choose-us card-of-choose-us">
+            <div class="card-of-choose-us__image-wrap">
+              <img
+                src="../image/keyword-research.png"
+                alt="Keyword Research"
+                class="card-of-choose-us__image"
+              />
+            </div>
+            <h4 class="card-of-choose-us__title">Keyword Research</h4>
+            <p class="card-of-choose-us__sign">
+              Business applications through revolutionary catalysts for chang
+              the Seamlessly underwhelm dures.
+            </p>
+            <a href="#" class="card-of-choose-us__button button">Learn more</a>
+          </div>
+
+          <div class="why-us__card-of-choose-us card-of-choose-us">
+            <div class="card-of-choose-us__image-wrap">
+              <img
+                src="../image/seo-optimization.png"
+                alt="SEO optimization"
+                class="card-of-choose-us__image"
+              />
+            </div>
+            <h4 class="card-of-choose-us__title">Seo Optimazadion</h4>
+            <p class="card-of-choose-us__sign">
+              Business applications through revolutionary catalysts for chang
+              the Seamlessly underwhelm dures.
+            </p>
+            <a href="#" class="card-of-choose-us__button button">Learn more</a>
+          </div>
+
+          <div class="why-us__card-of-choose-us card-of-choose-us">
+            <div class="card-of-choose-us__image-wrap">
+              <img
+                src="../image/social-marketing.png"
+                alt="Social marketing"
+                class="card-of-choose-us__image"
+              />
+            </div>
+            <h4 class="card-of-choose-us__title">Social marketing</h4>
+            <p class="card-of-choose-us__sign">
+              Business applications through revolutionary catalysts for chang
+              the Seamlessly underwhelm dures.
+            </p>
+            <a href="#" class="card-of-choose-us__button button">Learn more</a>
+          </div>
+        </div>
+				<div class="why-us-cards-mobile">
+					<carousel :data="[
+					<div class="why-us__card-of-choose-us card-of-choose-us">
+            <div class="card-of-choose-us__image-wrap">
+              <img
+                src="../image/keyword-research.png"
+                alt="Keyword Research"
+                class="card-of-choose-us__image"
+              />
+            </div>
+            <h4 class="card-of-choose-us__title">Keyword Research</h4>
+            <p class="card-of-choose-us__sign">
+              Business applications through revolutionary catalysts for chang
+              the Seamlessly underwhelm dures.
+            </p>
+            <a href="#" class="card-of-choose-us__button button">Learn more</a>
+          </div>,
+					<div class="why-us__card-of-choose-us card-of-choose-us">
+            <div class="card-of-choose-us__image-wrap">
+              <img
+                src="../image/seo-optimization.png"
+                alt="SEO optimization"
+                class="card-of-choose-us__image"
+              />
+            </div>
+            <h4 class="card-of-choose-us__title">Seo Optimazadion</h4>
+            <p class="card-of-choose-us__sign">
+              Business applications through revolutionary catalysts for chang
+              the Seamlessly underwhelm dures.
+            </p>
+            <a href="#" class="card-of-choose-us__button button">Learn more</a>
+          </div>,
+					<div class="why-us__card-of-choose-us card-of-choose-us">
+            <div class="card-of-choose-us__image-wrap">
+              <img
+                src="../image/social-marketing.png"
+                alt="Social marketing"
+                class="card-of-choose-us__image"
+              />
+            </div>
+            <h4 class="card-of-choose-us__title">Social marketing</h4>
+            <p class="card-of-choose-us__sign">
+              Business applications through revolutionary catalysts for chang
+              the Seamlessly underwhelm dures.
+            </p>
+            <a href="#" class="card-of-choose-us__button button">Learn more</a>
+          </div>]"></carousel>
+				</div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import InfiniteSlideBar from "vue-infinite-slide-bar";
+import VueCarousel from "@chenfengyuan/vue-carousel";
+
+export default {
+  components: {
+    InfiniteSlideBar,
+		VueCarousel,
+  },
+};
+</script>
